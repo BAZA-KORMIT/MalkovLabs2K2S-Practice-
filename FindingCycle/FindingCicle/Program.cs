2fc90331a30610e65c753e9a828761a9593b052f
TheRomaks <niks_2004@bk.ru>
2024-03-18 12:03:06 +0300

    FindinCycle finally fixed

--- FindingCycle/FindingCicle/Program.cs
+++ FindingCycle/FindingCicle/Program.cs
@@ -9,7 +9,7 @@ namespace FCG
 
         public static int[,] graph = {
                { 0, 1, 0, 0},
-               { 0, 0, 1, 0},
+               { 0, 0, 1, 1},
                { 0, 0, 0, 1},
                { 0, 0, 0, 0}
         };
@@ -23,7 +23,14 @@ namespace FCG
         {
             directed = IsDirected();
             Console.WriteLine("directed check: {0}", directed);
-            for (int i = 0; i < n; i++) // n - размерность матрицы
+            if (directed )
+                CycleD();
+            else
+                CycleND();
+        }
+        static void CycleD()
+        {
+            for (int i = 0; i < n; i++)
             {
                 if (DFS(i, used))
                 {
@@ -35,28 +42,29 @@ namespace FCG
             Console.WriteLine("В графе нет цикла.");
         }
 
-        static bool DFS(int v, Color[] used) // v - текущая вершина
+        static void CycleND()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (DFSND(i, used,-1))
+                {
+                    Console.WriteLine($"Цикл найден.");
+                    Console.WriteLine("Путь цикла: " + string.Join(" -> ", path));
+                    return;
+                }
+            }
+            Console.WriteLine("В графе нет цикла.");
+        }
+        static bool DFS(int v, Color[] used) 
         {
             path.Add(v + 1);
             used[v] = Color.GRAY;
             for (int i = 0; i < n; i++)
             {
-                if (directed)
+                if (graph[v, i] != 0)
                 {
-                    if (graph[v, i] != 0) // пофикси то что внутри
-                    {
-                        if (used[i] == Color.WHITE) return DFS(i, used);
-                        if (used[i] == Color.GRAY) { path.Add(i + 1);return true; }
-                    }
-                }
-                else
-                {
-                    if (graph[v, i] != 0)
-                    {
-                        if (used[i] == Color.WHITE) { previous[i] = v; return DFS(i, used); }
-                        if (used[i] == Color.GRAY && graph[i, v] == graph[v, i] && used[i] == used[v - 1]) continue;
-                        if (used[i] == Color.GRAY && previous[v] != i) { path.Add(i + 1); return true; }
-                    }
+                    if (used[i] == Color.WHITE && DFS(i, used)) return true;
+                    if (used[i] == Color.GRAY) { path.Add(i + 1); return true; }
                 }
             }
             used[v] = Color.BLACK;
@@ -64,6 +72,22 @@ namespace FCG
             return false;
         }
 
+        static bool DFSND(int v, Color[] used,int parent)
+        {
+            path.Add(v + 1);
+            used[v] = Color.GRAY;
+            for (int i = 0; i < n; i++)
+            {
+                if (graph[v, i] != 0) 
+                {
+                    if (used[i] == Color.WHITE && DFSND(i, used,v)) return true;
+                    if (used[i] == Color.GRAY && i!=parent) { path.Add(i + 1); return true; }
+                }
+            }
+            used[v] = Color.BLACK;
+            path.RemoveAt(path.Count - 1);
+            return false;
+        }
         public static bool IsDirected()
         {
             for (int i = 0; i < n; i++)
--- FindingCycle/FindingCicle/obj/Debug/net8.0/FindingCicle.AssemblyInfo.cs
+++ FindingCycle/FindingCicle/obj/Debug/net8.0/FindingCicle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FindingCicle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686368bb4deb55e31f944b6e237f4de1e4f4a780")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b45546689d99921e26908919190daac5f48f5372")]
 [assembly: System.Reflection.AssemblyProductAttribute("FindingCicle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FindingCicle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
