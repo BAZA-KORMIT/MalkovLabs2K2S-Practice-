7a8c77ca35ad60f360ff95824fd00a423231a5df
Владислав <vlad2004ki@gmail.com>
2024-04-10 11:44:38 +0300

    add new pract
    в чо за бизнес сука надо дописать алгоритм основанный на потоках будь то Форда-Ф алгоритм или же эдмонса карпа

---
+++ Chto-za-biznes/Chto-za-biznes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Chto_za_biznes;
+
+class Program
+{
+    static void Main()
+    {
+        int N = 2; // Количество вышек
+        int M = 2; // Количество заводов
+        int[] a = { 10000, 20000 }; // Производительность вышек
+        int[] b = { 15000, 15000 }; // Производительность заводов
+        int[,] c = { { 500, 900 }, { 600, 700 } }; // Стоимость транспортировки
+
+        // Создание графа
+        int[,] graph = new int[N + M + 2, N + M + 2];
+
+        // Инициализация графа
+        for (int i = 0; i < a.Length; i++)
+        {
+            graph[0, i + 1] = a[i]; // Связь источника с вышками
+        }
+
+        for (int i = 0; i < b.Length; i++)
+        {
+            graph[N + 1 + i, N + M + 1] = b[i]; // Связь заводов со стоком
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                graph[i + 1, N + 1 + j] = int.MaxValue; // Максимальная пропускная способность
+            }
+        }
+
+
+    }
+
+    //static bool CanConnect(int i, int j) => a[i] <= b[j];
+    //static int GetPrice(int i, int j) => c[i, j];
+
+    static void MaxFlowLowCost()
+    {
+       
+    }
+}
---
+++ CitiesAndRoads/CitiesAndRoads/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Collections.Generic;
+
+namespace CitiesAndRoads;
+
+public class RoadsAndCities
+{
+    private static int[,] _adjMatrix;
+    private static bool[] _visited;
+
+    public static int FindComponents(int n, Tuple<int, int>[] edges)
+    {
+        _adjMatrix = new int[n + 1, n + 1];
+
+
+        foreach (var edge in edges)
+        {
+            _adjMatrix[edge.Item1,edge.Item2] = 1;
+            _adjMatrix[edge.Item2,edge.Item1] = 1;
+        }
+
+        _visited = new bool[n + 1];
+        int components = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (!_visited[i])
+            {
+                DFS(i);
+                components++;
+            }
+        }
+        return components - 1;
+    }
+
+    private static void DFS(int i)
+    {
+        _visited[i] = true; 
+        //Console.WriteLine("Посещена вершина " + i); 
+        for (int j = 0; j < _adjMatrix.GetLength(1); j++)
+        {
+            if (_adjMatrix[i, j] == 1 && !_visited[j])
+            {
+                DFS(j);
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int N = 6; 
+        Tuple<int, int>[] edges = {
+            Tuple.Create(1, 2),
+            Tuple.Create(1, 3),
+            Tuple.Create(4, 5),
+        };
+
+        Console.WriteLine($"Минимальное количество новых дорог: {RoadsAndCities.FindComponents(N, edges)}");
+    }
+}
